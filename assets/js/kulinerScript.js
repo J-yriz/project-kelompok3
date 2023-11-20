cd1195c452ff98164d85d277ff2495e57bb2a1aa
Jyriz <discordketukangan123@gmail.com>
2023-11-20 21:59:39 +0700

    updateCard, updateModalDetail, updateStrukturJSON

--- assets/js/kulinerScript.js
+++ assets/js/kulinerScript.js
@@ -157,6 +157,48 @@ xhttp.onreadystatechange = function () {
                 button.style.display = "none";
             }
         });
+
+        // ModalDetail
+        const kartuPro = document.querySelectorAll('.kartuProduk');
+        const modalContent = document.querySelector('.modalContent')
+
+        kartuPro.forEach((elemen) => {
+            elemen.addEventListener('click', function () {
+                const dataDetail = Number(elemen.getAttribute('data-detail'));
+                data.forEach((e) => {
+                    if (e.id === dataDetail) {
+                        let modalDetailMakanan = `
+                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                            <p class="text-xl font-semibold text-gray-900 dark:text-white flex items-center">
+                            <a style="margin-right: 5px;" href="${e.detail.linkWebsite}" target="_blank"">${e.namaProduk} |</a>
+                            <span class="small">Rp.</span> ${e.harga.replace(/\B(?=(\d{3})+(?!\d))/g, ".")}
+                            </p>
+                        </div>
+                        <!-- Modal body -->
+                        <div class="flex flex-col md:flex-row p-4 md:p-5 space-y-4 md:space-y-0 md:space-x-4 items-center">
+                            <img src="./assets/images/${e.gambar}" alt="product image" class="w-30 sm:w-2/5" loading="lazy" />
+                            <div class="penjelasanMakanan text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                            <div class="keteranganMakanan border p-2 md:p3 md:rounded-md">
+                                <div class="kotakAsikSendiri border-b flex flex-col md:flex-row pb-2">
+                                <p class="mr-4"><span class="font-semibold text-white">Rating Menu</span>${e.bintang}</p>
+                                <p><span class="font-semibold text-white">Keterangan</span>${e.keterangan}</p>
+                                </div>
+                                <p class="border-b pb-2"><span class="font-semibold text-white">Alamat</span>${e.detail.alamat}</p>
+                                <p><span class="font-semibold text-white">Menu lainnya</span>- ${e.detail.menu1} | Rp.${e.detail.harga1.replace(/\B(?=(\d{3})+(?!\d))/g, ".")}<br>- ${e.detail.menu2} | Rp.${e.detail.harga2.replace(/\B(?=(\d{3})+(?!\d))/g, ".")}<br>- ${e.detail.menu3} | Rp.${e.detail.harga3.replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</p>
+                            </div>
+                            </div>
+                        </div>
+                        <iframe
+                            src="${e.detail.linkMap}"
+                            loading="lazy" class="px-4 md:px-5 w-full h-80 md:h-56" frameborder="0">
+                        </iframe>`
+                        
+                        modalContent.innerHTML = modalDetailMakanan;
+                    }
+                })
+            });
+        });
+
     }
 };
 xhttp.open("GET", "assets/json/data.json", true);
@@ -167,7 +209,7 @@ function isiHtmlCards(e, s) {
         { length: e.bintang },
         () => '<ion-icon name="star"></ion-icon>'
     ).join("");
-    return `<a class="kartuProduk" ${s} data-modal-target="modalDetailMakanan" data-modal-toggle="modalDetailMakanan">
+    return `<a class="kartuProduk" ${s} data-modal-target="modalDetailMakanan" data-modal-toggle="modalDetailMakanan" data-detail=" ${e.id}">
                 <div class="product-card">
                 <div class="img-box">
                     <img
@@ -183,7 +225,7 @@ function isiHtmlCards(e, s) {
                     <div class="wrapper">
                     <h3 class="product-name">${e.namaProduk}</h3>
 
-                    <p class="product-price">${e.harga}</p>
+                    <p class="product-price"><span class='small'>Rp.</span>${e.harga.replace(/\B(?=(\d{3})+(?!\d))/g, ".")}</p>
                     </div>
 
                     <p class="product-text">
--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -1,13 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 tailwind.config = {
     content: ["./.{html}"],
+    darkMode: 'class',
     theme: {
         extend: {
             fontFamily: {
                 'rubik' : ["Rubik", "sans-serif"]
             },
             colors: {
-                'biruMiaw' : '#86D6F2'
+                'biruMiaw' : '#86D6F2',
+                'putihMiaw': '#F6F1F1',
+                'hitamMiaw': '#241f42'
             }
         }
     }
